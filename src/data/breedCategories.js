64fb579b2b9aa1f8d20008b0b375194d492fbfcf
FilipStefanovski1 <fikokapo031@gmail.com>
2026-05-06 00:22:59 +0200

    add breed category data

---
+++ src/data/breedCategories.js
@@ -0,0 +1,56 @@
+export const sizeMap = {
+  Small: [
+    'affenpinscher', 'cairn', 'chihuahua', 'cotondetulear', 'dachshund',
+    'frise', 'havanese', 'lhasa', 'maltese', 'mexicanhairless', 'papillon',
+    'pekinese', 'pomeranian', 'pug', 'schipperke', 'shiba', 'shihtzu'
+  ],
+  Medium: [
+    'basenji', 'basset', 'beagle', 'bluetick', 'buhund', 'bulldog',
+    'bullterrier', 'cattledog', 'chow', 'clumber', 'collie', 'coonhound',
+    'corgi', 'dalmatian', 'dhole', 'dingo', 'elkhound', 'entlebucher',
+    'finnish', 'groenendael', 'keeshond', 'kelpie', 'malinois', 'otterhound',
+    'pembroke', 'pitbull', 'pointer', 'poodle', 'samoyed', 'schnauzer',
+    'setter', 'sharpei', 'sheepdog', 'spaniel', 'springer', 'terrier',
+    'vizsla', 'waterdog', 'weimaraner', 'whippet'
+  ],
+  Large: [
+    'african', 'airedale', 'akita', 'appenzeller', 'australian', 'borzoi',
+    'bouvier', 'boxer', 'brabancon', 'briard', 'dane', 'deerhound', 'doberman',
+    'eskimo', 'germanshepherd', 'greyhound', 'hound', 'husky', 'komondor',
+    'kuvasz', 'labrador', 'leonberg', 'malamute', 'mastiff', 'mountain',
+    'newfoundland', 'ovcharka', 'pyrenees', 'redbone', 'retriever', 'ridgeback',
+    'rottweiler', 'saluki', 'stbernard', 'wolfhound'
+  ]
+}
+
+export const continentMap = {
+  Europe: [
+    'affenpinscher', 'airedale', 'appenzeller', 'basset', 'beagle', 'borzoi',
+    'bouvier', 'boxer', 'brabancon', 'briard', 'buhund', 'bulldog', 'bullterrier',
+    'cairn', 'clumber', 'collie', 'corgi', 'dachshund', 'dalmatian', 'dane',
+    'deerhound', 'doberman', 'elkhound', 'entlebucher', 'finnish', 'frise',
+    'germanshepherd', 'greyhound', 'groenendael', 'keeshond', 'komondor',
+    'kuvasz', 'leonberg', 'malinois', 'maltese', 'mastiff', 'mountain',
+    'otterhound', 'papillon', 'pembroke', 'pinscher', 'pointer', 'pomeranian',
+    'poodle', 'pyrenees', 'retriever', 'rottweiler', 'samoyed', 'schipperke',
+    'schnauzer', 'setter', 'sheepdog', 'spaniel', 'springer', 'stbernard',
+    'terrier', 'vizsla', 'waterdog', 'weimaraner', 'whippet', 'wolfhound'
+  ],
+  Asia: [
+    'akita', 'chow', 'dhole', 'lhasa', 'pekinese', 'pug', 'saluki',
+    'sharpei', 'shiba', 'shihtzu'
+  ],
+  'North America': [
+    'bluetick', 'chihuahua', 'coonhound', 'eskimo', 'husky', 'labrador',
+    'malamute', 'mexicanhairless', 'newfoundland', 'pitbull', 'redbone'
+  ],
+  'South America': [
+    'havanese'
+  ],
+  Africa: [
+    'african', 'basenji', 'cotondetulear', 'ridgeback'
+  ],
+  Oceania: [
+    'australian', 'cattledog', 'dingo', 'kelpie'
+  ]
+}
